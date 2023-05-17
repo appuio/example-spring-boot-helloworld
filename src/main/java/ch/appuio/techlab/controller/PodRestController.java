fa13ad4b4a054687b49d93fce4171ef3e43bf9b6
Christoph Raaflaub <raaflaub@puzzle.ch>
2023-05-17 15:18:54 +0200

    make it backward compatible

--- src/main/java/ch/appuio/techlab/controller/PodRestController.java
+++ src/main/java/ch/appuio/techlab/controller/PodRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pod/")
+@RequestMapping({"/pod", "/pod/"})
 public class PodRestController {
 	
 	@RequestMapping(method = RequestMethod.GET)
